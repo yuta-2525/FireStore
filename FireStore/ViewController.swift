19ce4ad6d2e42ac05866911e713cbc325102fa02
yuta-2525 <Yuta1091.3150@gmail.com>
2021-11-26 15:07:55 +0900

    Initial Commit

---
+++ FireStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireStore
+//
+//  Created by 佐藤勇太 on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
